a3ddfeb9245803c83834a793a34e493eb0ac3bd6
Steve Shipsey <Steven.Shipsey@novelis.com>
2014-10-31 10:34:24 -0400

    cleanup

--- songs.py
+++ songs.py
@@ -12,30 +12,32 @@ textregex = re.compile("(?<=\[)[^\]]*(?=\])")
 
 f = open('Playlist.html', 'w')
 
-f.write('<div><ul>')
+f.write('<body style="background-color: #b0c4de"><h1>Reddit\'s Playlist:</h1><div>')
 
 i = 0
 j = 0
 
 for thread in threads:
-    f.write('<li>' + thread.title + '</li>')
     j = 0
+    itemstring = ''
     for comment in thread.comments:
         if not hasattr(comment, 'body'):
             continue
         match = linkregex.search(comment.body)
         text = textregex.search(comment.body)
+        titlestring = '<div style="font-size:110%"><b>' + thread.title + '</b></div><ul>'
         if match and text:
-            #print match
-            #print text
-            string = '<li><a href=' + match.group() + '>' + text.group() + '</a></li>'
-            string = string.encode('ascii','ignore')
-            f.write(string)
+            itemstring += '<li><a href=' + match.group() + '>' + text.group() + '</a></li>'
         j += 1
         if j >= 20:
         	break
+    if itemstring:
+    	f.write(titlestring)
+    	itemstring = itemstring.encode('ascii','ignore')
+    	f.write(itemstring)
     i += 1
+    f.write('</ul>')
     if i >= 10:
     	break
-f.write('</ul></div>')
+f.write('</ul></div></body>')
 f.close()
