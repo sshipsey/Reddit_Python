c7edf0d5b076e4d228215d5d9d57d6ef785220f4
Steve Shipsey <Steven.Shipsey@novelis.com>
2014-10-31 09:48:53 -0400

    Added creation of an html file with songs by name

--- songs.py
+++ songs.py
@@ -5,15 +5,37 @@ user_agent = "Song finder bot (read only) by /u/XiiencE"
 api = praw.Reddit(user_agent=user_agent)
 
 threads = api.search("songs that", subreddit="askreddit", limit=10)
-# http://blog.lucascaton.com.br/index.php/2012/04/10/regex-to-match-youtube-urls-using-ruby/
-regex = re.compile("(?:https?:\/\/)?(?:www\.)?youtu(?:\.be|be\.com)\/(?:watch\?v=)?([\w-]{10,})")
+
+linkregex = re.compile("(?:https?:\/\/)?(?:www\.)?youtu(?:\.be|be\.com)\/(?:watch\?v=)?([\w-]{10,})")
+
+textregex = re.compile("(?<=\[)[^\]]*(?=\])")
+
+f = open('Playlist.html', 'w')
+
+f.write('<div><ul>')
+
+i = 0
+j = 0
 
 for thread in threads:
-    print thread.title
+    f.write('<li>' + thread.title + '</li>')
+    j = 0
     for comment in thread.comments:
-        # http://stackoverflow.com/questions/17430409/in-praw-im-trying-to-print-the-comment-body-but-what-if-i-encounter-an-empty
         if not hasattr(comment, 'body'):
             continue
-        match = regex.search(comment.body)
-        if match:
-            print match.group()
+        match = linkregex.search(comment.body)
+        text = textregex.search(comment.body)
+        if match and text:
+            #print match
+            #print text
+            string = '<li><a href=' + match.group() + '>' + text.group() + '</a></li>'
+            string = string.encode('ascii','ignore')
+            f.write(string)
+        j += 1
+        if j >= 20:
+        	break
+    i += 1
+    if i >= 10:
+    	break
+f.write('</ul></div>')
+f.close()
